d8330fb9bcd22c1bee56eb15eb0fa8c84706423e
Rupak <rupakdas.dev@gmail.com>
2024-07-14 09:56:22 +0530

    Initial Commit

---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Rupak Das on 14/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
